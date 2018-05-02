fe900effa7a20e1aca6c5b6189b95055a240d653
dmculley <daveculley@improbable.io>
2018-05-02 16:17:39 +0100

    Git didn't add fieldParams

---
+++ finiteDifference/src/main/kotlin/io/improbable/swayze/finiteDifference/FieldParams.kt
@@ -0,0 +1,43 @@
+package io.improbable.swayze.finiteDifference
+
+import io.improbable.keanu.kotlin.ArithmeticDouble
+
+class FieldParams(var XSIZE: Int, var YSIZE: Int, var DX: Double, var DY: Double) {
+
+    var DT = 0.0
+
+    var TLENGTH = 0.0
+    var maxIteration = 0
+    var targetError = 0.0
+    var numberTimesteps = 0
+
+    var t = 0.0
+    var iteration = 0
+    var innerIteration = 0
+    var currentError = Double.MAX_VALUE
+
+    var dumpFrequency = 0
+
+    // Physical constants
+    var nu = 0.1
+    var rho = 1.0
+    var c = 1.0
+
+    // Ones domain - for building obstructions
+    var onesDomain = Domain(DX, DY,
+            Boundary.Dirichlet(ArithmeticDouble(1.0)),
+            Boundary.Dirichlet(ArithmeticDouble(1.0)),
+            Boundary.Dirichlet(ArithmeticDouble(1.0)),
+            Boundary.Dirichlet(ArithmeticDouble(1.0)))
+
+    constructor(XSIZE: Int, YSIZE: Int, TLENGTH: Double, DX: Double, DY: Double, DT: Double):
+            this(XSIZE, YSIZE, DX, DY) {
+        this.DT = DT
+        this.TLENGTH = TLENGTH
+    }
+
+    constructor(XSIZE: Int, YSIZE: Int, DX: Double, DY: Double, targetError: Double):
+            this(XSIZE, YSIZE, DX, DY) {
+        this.targetError = targetError
+    }
+}
